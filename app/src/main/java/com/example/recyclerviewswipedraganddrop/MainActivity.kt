66b44f5c31f3cb0d2754231b8143f1f31a85969e
User <kenjaxojayevnurulloxon@gmail.com>
2022-10-03 01:47:21 -0700

    For Beginners

--- app/src/main/java/com/example/recyclerviewswipedraganddrop/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewswipedraganddrop/MainActivity.kt
@@ -2,36 +2,15 @@ package com.example.recyclerviewswipedraganddrop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.recyclerviewswipedraganddrop.adapters.MyDiffUtilAdapter
 import com.example.recyclerviewswipedraganddrop.databinding.ActivityMainBinding
-import com.example.recyclerviewswipedraganddrop.models.User
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var userList: ArrayList<User>
-    private lateinit var adapter: MyDiffUtilAdapter
 
-    private var count: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        userList = ArrayList()
-        adapter = MyDiffUtilAdapter()
-        adapter.submitList(userList)
-        binding.recyclerView.adapter = adapter
-
-
-        binding.save.setOnClickListener {
-            val name: String = binding.name.text.toString()
-            val password: String = binding.password.text.toString()
-
-            val user = User(count++, name, password)
-            userList.add(user)
-            adapter.submitList(userList)
-            binding.recyclerView.adapter = adapter
-        }
     }
 }
